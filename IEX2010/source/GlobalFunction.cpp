375cb04e9ba5ed99ae78579203dd9a0dd437ea03
高鳥義行 <wtpmjgdadgjptw@gmail.com>
2016-07-12 22:59:27 +0900

    UIManagerクラス作成 GameManagerクラス作成 GlobalFunction修正

--- IEX2010/source/GlobalFunction.cpp
+++ IEX2010/source/GlobalFunction.cpp
@@ -1,5 +1,7 @@
 
 #include	"iextreme.h"
+//#include	"Camera.h"
+//#include	"DrawShape.h"
 #include	"GlobalFunction.h"
 
 //*****************************************************************************
@@ -7,12 +9,6 @@
 //	グローバル関数
 //
 //*****************************************************************************
-
-//----------------------------------------------------------------------------
-//	変数
-//----------------------------------------------------------------------------
-
-#define	PERCENTAGE_MAX	1.0f
 
 //----------------------------------------------------------------------------
 //	システム
@@ -80,447 +76,70 @@
 		return true;
 	}
 
-	//	クライアント座標からワールド座標へ
-	void	ClientToWorld( const Vector3& screenPos, Vector3& out )
+	//	２D座標から３D座標への変換
+	void	TransCoordScreenToWorld( const Vector3& screenPos, Vector3& out )
 	{
 		D3DXVECTOR3	d3dxScreenCoord( screenPos.x, screenPos.y, screenPos.z );
 
+		//	ビューポート
 		D3DVIEWPORT9	viewPort;
 		iexSystem::Device->GetViewport( &viewPort );
 
+		//	ワールド変換
 		D3DXMATRIX	worldMatrix;
 		D3DXMatrixIdentity( &worldMatrix );
+
+		//	変換
 		D3DXVECTOR3	result;
 		D3DXVec3Unproject( &result, &d3dxScreenCoord, &viewPort, &matProjection, &matView, &worldMatrix );
-
+		
+		//	出力
 		out = Vector3( result.x, result.y, result.z );
 	}
 
-	//	モーション番号登録
-	void	SetMotionNum( int& motionData, int motionNum )
+	//	スクリーン座標からワールド座標へ変換
+	void	ClientToWorld( Vector3& outPos, Vector3& outVec, POINT screenPos )
 	{
-		motionData = motionNum;
+		//	スクリーン座標からワールド座標を求める
+		Vector3	nearPoint;
+		TransCoordScreenToWorld( Vector3( ( float )screenPos.x, ( float )screenPos.y, 0.1f ), nearPoint );
+		Vector3	farPoint;
+		TransCoordScreenToWorld( Vector3( ( float )screenPos.x, ( float )screenPos.y, 0.9f ), farPoint );
+		outPos = nearPoint;
+		Vector3	vEye = farPoint - nearPoint;
+		vEye.Normalize();
+
+		outVec = vEye;
 	}
 
-	//	デバッグ文字
-	void	DrawString( LPSTR string, int x, int y, DWORD color )
+	//	スクリーン座標からワールド座標へ変換（ レイを当てるメッシュ、出力、スクリーン座標 ）
+	void	ClientToWorld( iexMesh* targetMesh, Vector3& out, const POINT& screenPos )
 	{
-		IEX_DrawText( string, x, y, 300, 300, color );
-	}
-	
-	void	DrawString( LPSTR string, int x, int y, float r, float g, float b )
-	{
-		IEX_DrawText(string, x, y, 300, 300, ARGB(0xFF, ( DWORD )( 0xFF * r ), ( DWORD )( 0xFF * g ), ( DWORD )( 0xFF * b ) ) );
-	}
-	
-	void	DrawString( LPSTR string, int x, int y, Vector3 color )
-	{
-		IEX_DrawText( string, x, y, 300, 300, ARGB( 0xFF, ( DWORD )( 0xFF * color.x ), ( DWORD )( 0xFF * color.y ), ( DWORD )( 0xFF * color.z ) ) );
+		//	変数準備
+		Vector3	pos, vec;
+		float dist = FLT_MAX;
+
+		//	ワールド座標を求める
+		ClientToWorld( pos, vec, screenPos );
+
+		//	メッシュへレイを飛ばし、当たった座標をかえす
+		targetMesh->RayPick( &out, &pos, &vec, &dist );
 	}
 
-//----------------------------------------------------------------------------
-//	画像関連
-//----------------------------------------------------------------------------
-
-	//	初期化
-	void	ImageInitialize(ImageObj& image, int x, int y, int w, int h, int sx, int sy, int sw, int sh)
+	//	前フレームからの経過時間取得
+	float	GetElapseTime( void )
 	{
-		image.x = x;
-		image.y = y;
-		image.w = w;
-		image.h = h;
-		image.sx = sx;
-		image.sy = sy;
-		image.sw = sw;
-		image.sh = sh;
-		image.plusScaleX = 0;
-		image.plusScaleY = 0;
-		image.t = 0.0f;
-		image.alpha = 1.0f;
-		image.angle = 0.0f;
-		image.color = Vector3(1.0f, 1.0f, 1.0f);
-		image.p = GetPoint(image.x, image.y);
-		image.renderflag = true;
-
-		//	wave用パラメータ
-		image.waveAlpha = 1.0f;
-		image.waverenderflag = false;
-		image.wavespeed = 0.0f;
-		image.waveState = false;
-
-		//	flashing用パラメータ
-		image.flashingSpeed = 0.0f;
-		image.flashingAlpha = 0.0f;
-		image.flashingParam = 0.0f;
-		image.flashingRenderflag = true;
-
-		//	scaling用パラメータ
-		image.scalingspeed = 0.0f;
-		image.scalingAlpha = 0.0f;
-		image.scalingState = true;
-		image.scalingFlag = false;
-		image.scalingrenderflag = true;
-	}
-
-	//	画像(位scalingrenderflag;置はイメージの値)
-	void	RenderImage( ImageObj image, int sx, int sy, int sw, int sh, int mode )
-	{
-		int		width = image.w;
-		int		height = image.h;
-		int		posx = image.x - width / 2;
-		int		posy = image.y - height / 2;
-
-		switch ( mode )
-		{
-		case IMAGE_MODE::NORMAL:
-			if ( image.renderflag )
-			image.obj->Render( posx, posy, width, height, sx, sy, sw, sh );
-			break;
-
-		case IMAGE_MODE::ADOPTPARAM:
-			if ( image.renderflag )
-				image.obj->Render( posx, posy, width, height, sx, sy, sw, sh, image.p, image.angle, RS_COPY, GetColor( image.color, image.alpha ) );
-			break;
-
-		case IMAGE_MODE::ADD:
-			if (image.renderflag)
-				image.obj->Render(posx, posy, width, height, sx, sy, sw, sh, image.p, image.angle, RS_ADD, GetColor(image.color, image.alpha));
-			break;
-
-		case IMAGE_MODE::WAVE:
-			width = image.w + image.plusScaleX;
-			height = image.h + image.plusScaleY;
-			posx = image.x - width / 2;
-			posy = image.y - height / 2;
-
-			if ( image.waverenderflag )
-			image.obj->Render( posx, posy, width, height, sx, sy, sw, sh, image.p, image.angle, RS_COPY, GetColor( image.color, image.waveAlpha ) );
-			break;
-
-		case IMAGE_MODE::FLASH:
-			if ( image.flashingRenderflag )
-				image.obj->Render( posx, posy, width, height, sx, sy, sw, sh, RS_COPY, GetColor( image.color, image.flashingAlpha ) );
-			break;
-
-		case IMAGE_MODE::SCALING:
-			width = image.w + image.plusScaleX;
-			height = image.h + image.plusScaleY;
-			posx = image.x - width / 2;
-			posy = image.y - height / 2;
-
-			if ( image.scalingrenderflag )
-				image.obj->Render( posx, posy, width, height, sx, sy, sw, sh, image.p, image.angle, RS_COPY, GetColor( image.color, image.alpha ) );
-			break;
-		}
-	}
-
-	//	画像(位置をイメージとは違う引数の値を与える)
-	void	RenderImage(ImageObj image, int sx, int sy, int sw, int sh, int mode, int x, int y)
-	{
-		int		width = image.w;
-		int		height = image.h;
-		int		posx = x - width / 2;
-		int		posy = y - height / 2;
-
-		switch (mode)
-		{
-		case IMAGE_MODE::NORMAL:
-			if (image.renderflag)
-				image.obj->Render(posx, posy, width, height, sx, sy, sw, sh);
-			break;
-
-		case IMAGE_MODE::ADOPTPARAM:
-			if (image.renderflag)
-				image.obj->Render(posx, posy, width, height, sx, sy, sw, sh, image.p, image.angle, RS_COPY, GetColor(image.color, image.alpha));
-			break;
-
-		case IMAGE_MODE::ADD:
-			if (image.renderflag)
-				image.obj->Render(posx, posy, width, height, sx, sy, sw, sh, image.p, image.angle, RS_ADD, GetColor(image.color, image.alpha));
-			break;
-
-		case IMAGE_MODE::WAVE:
-			width = image.w + image.plusScaleX;
-			height = image.h + image.plusScaleY;
-			posx = x - width / 2;
-			posy = y - height / 2;
-
-			if (image.waverenderflag)
-				image.obj->Render(posx, posy, width, height, sx, sy, sw, sh, RS_COPY, GetColor(image.color, image.waveAlpha));
-			break;
-
-		case IMAGE_MODE::FLASH:
-			if (image.flashingRenderflag)
-				image.obj->Render(posx, posy, width, height, sx, sy, sw, sh, RS_COPY, GetColor(image.color, image.flashingAlpha));
-			break;
-
-		case IMAGE_MODE::SCALING:
-			width = image.w + image.plusScaleX;
-			height = image.h + image.plusScaleY;
-			posx = x - width / 2;
-			posy = y - height / 2;
-
-			if ( image.scalingrenderflag )
-				image.obj->Render( posx, posy, width, height, sx, sy, sw, sh, image.p, image.angle, RS_COPY, GetColor( image.color, image.alpha ) );
-			break;
-		}
-	}
-
-	//	波紋設定
-	void	SetWave( ImageObj& image, float speed )
-	{
-		image.plusScaleX = 0;
-		image.plusScaleY = 0;
-		image.t = 0;
-		image.waveAlpha = 1.0f;
-		image.waveState = true;
-		image.wavespeed = speed;
-		image.waverenderflag = true;
-	}
-
-	//	波紋更新
-	bool	WaveUpdate( ImageObj& image, int max_scale, float max_alpha )
-	{
-		if ( image.waveState )
-		{
-			//	パラメータ加算
-			image.t += D3DX_PI / 180 * image.wavespeed;
-
-			//	パラメータ上限設定
-			if ( image.t >= 1.0f )
-			{
-				image.t = 1.0f;
-				image.waveState = false;
-				image.waverenderflag = false;
-			}
-
-			Lerp( image.waveAlpha, max_alpha, 0.0f, image.t );
-			Lerp( image.plusScaleX, 0, max_scale, image.t );
-			Lerp( image.plusScaleY, 0, max_scale, image.t );
-
-		}
-
-		if ( image.t >= 1.0f )		return	true;
-		return	false;
-	}
-
-	//	拡大縮小終了
-	void	StopScaling( ImageObj& image )
-	{
-		image.plusScaleX = 0;
-		image.plusScaleY = 0;
-		image.t = 0;
-		image.alpha = 1.0f;
-		image.scalingState = true;
-		image.scalingspeed = 0.0f;
-		image.scalingFlag = false;
-		image.scalingrenderflag = true;
-	}
-
-	//	拡大縮小設定
-	void	SetScaling(ImageObj& image, float speed, bool state)
-	{
-		image.plusScaleX = 0;
-		image.plusScaleY = 0;
-		image.t = 0;
-		if (state)	image.alpha = 1.0f;
-		if (!state)	image.alpha = 0.0f;
-		image.scalingAlphaFlag = state;
-		image.scalingState = true;
-		image.scalingspeed = speed;
-		image.scalingFlag = true;
-		image.scalingrenderflag = true;
-	}
-
-	//	拡大縮小更新
-	void	ScalingUpdate(ImageObj& image, int max_scale)
-	{
-		if (!image.scalingFlag) return;
-
-		//	パラメータ加算
-		image.t += D3DX_PI / 180 * image.scalingspeed;
-	
-		//-------------------------
-		//	拡大
-		//-------------------------
-		if (image.scalingState)
-		{
-			//	パラメータ上限設定
-			if (image.t >= 1.0f)
-			{
-				image.t = 1.0f;
-				image.scalingState = false;
-			}
-
-			Lerp(image.plusScaleX, 0, max_scale, image.t);
-			Lerp(image.plusScaleY, 0, max_scale, image.t);
-
-		}
-
-		//-------------------------
-		//	縮小
-		//-------------------------
-		else
-		{
-			//	パラメータ上限設定
-			if (image.t >= 1.0f)
-			{
-				image.t = 1.0f;
-				image.scalingState = true;
-			}
-
-			Lerp(image.plusScaleX, max_scale, 0, image.t);
-			Lerp(image.plusScaleY, max_scale, 0, image.t);
-
-		}
-
-		if (image.t >= 1.0f)		image.t = 0.0f;
-	}
-
-	//	拡大縮小更新
-	bool	ScalingAlphaUpdate(ImageObj& image, int max_scale)
-	{
-		if (!image.scalingFlag) return false;
-
-		switch ( image.scalingAlphaFlag )
-		{
-		case true:
-			//	パラメータ加算
-			image.t += D3DX_PI / 180 * image.scalingspeed;
-			image.alpha -= D3DX_PI / 180 * image.scalingspeed;
-
-			//-------------------------
-			//	拡大
-			//-------------------------
-			//	パラメータ上限設定
-			if (image.t >= 1.0f)
-			{
-				image.t = 0.0f;
-				image.alpha = 0.0f;
-				image.scalingFlag = false;
-				return true;
-			}
-
-			Lerp(image.plusScaleX, 0, max_scale, image.t);
-			Lerp(image.plusScaleY, 0, max_scale, image.t);
-
-			if (image.alpha <= 0.0f) image.alpha = 0.0f;
-			return false;
-
-		case false:
-			//	パラメータ加算
-			image.t += D3DX_PI / 180 * image.scalingspeed;
-			image.alpha += D3DX_PI / 180 * image.scalingspeed;
-
-			//-------------------------
-			//	縮小
-			//-------------------------
-			//	パラメータ上限設定
-			if (image.t >= 1.0f)
-			{
-				image.t = 0.0f;
-				image.alpha = 1.0f;
-				image.scalingFlag = false;
-				return true;
-			}
-
-			Lerp(image.plusScaleX, max_scale, 0, image.t);
-			Lerp(image.plusScaleY, max_scale, 0, image.t);
-
-			if (image.alpha >= 1.0f) image.alpha = 1.0f;
-			return false;
-			break;
-		}
-		return false;
-	}
-
-	//	横拡大縦縮小更新
-	void	ScalingLandingUpdate( ImageObj& image, int max_scale )
-	{
-		if (!image.scalingFlag) return;
-
-		//	パラメータ加算
-		image.t += D3DX_PI / 180 * image.scalingspeed;
-	
-		//-------------------------
-		//	拡大
-		//-------------------------
-		if (image.scalingState)
-		{
-			//	パラメータ上限設定
-			if (image.t >= 1.0f)
-			{
-				image.t = 1.0f;
-				image.scalingState = false;
-			}
-
-			Lerp(image.plusScaleX, 0, max_scale, image.t);
-			Lerp(image.plusScaleY, 0, -max_scale, image.t);
-
-		}
-
-		//-------------------------
-		//	縮小
-		//-------------------------
-		else
-		{
-			//	パラメータ上限設定
-			if (image.t >= 1.0f)
-			{
-				image.t = 1.0f;
-				image.scalingState = true;
-				image.scalingFlag = false;
-			}
-
-			Lerp(image.plusScaleX, max_scale, 0, image.t);
-			Lerp(image.plusScaleY, -max_scale, 0, image.t);
-
-		}
-
-		if (image.t >= 1.0f)		image.t = 0.0f;
-	}
-
-
-	//	点滅処理
-	void	FlashingUpdate( ImageObj& image, float speed )
-	{
-		if ( speed > 0.0f )
-			image.flashingParam += speed;
-		else
-			image.flashingParam += image.flashingSpeed;
-
-		if ( image.flashingParam >= D3DX_PI * 2.0f )	image.flashingParam = 0.0f;
-
-		//	透明度更新
-		image.flashingAlpha = 0.5f + 0.5f * sinf( image.flashingParam );
-	}
-
-//----------------------------------------------------------------------------
-//	ベジェ曲線( 出力、始点、制御点、終点、現在の割合( 0.0f ~ 1.0f ) )
-//----------------------------------------------------------------------------
-
-	//	２次ベジェ曲線
-	bool	BezierCurve( Vector3& out, Vector3 start, Vector3 p1, Vector3 end, float t )
-	{
-		if ( t >= 1.0f )	return true;
-
-		out.x = ( ( 1 - t ) * ( 1 - t ) ) * start.x + ( ( 2 * ( 1 - t ) * t * p1.x ) + ( ( t * t ) * end.x ) );
-		out.y = ( ( 1 - t ) * ( 1 - t ) ) * start.y + ( ( 2 * ( 1 - t ) * t * p1.y ) + ( ( t * t ) * end.y ) );
-		out.z = ( ( 1 - t ) * ( 1 - t ) ) * start.z + ( ( 2 * ( 1 - t ) * t * p1.z ) + ( ( t * t ) * end.z ) );
-
-		return	false;
-	}
-	
-	//	３次ベジェ曲線
-	bool	BezierCurve( Vector3& out, Vector3 start, Vector3 p1, Vector3 p2, Vector3 end, float t )
-	{
-		if ( t >= 1.0f )	return true;
-
-		out.x = ( ( 1 - t ) * ( 1 - t ) * ( 1 - t ) ) * start.x + ( 3 * ( ( 1 - t ) * ( 1 - t ) ) * t * p1.x ) + ( 3 * ( 1 - t ) * ( t * t ) * p2.x ) + ( t * t * t ) * end.x;
-		out.y = ( ( 1 - t ) * ( 1 - t ) * ( 1 - t ) ) * start.y + ( 3 * ( ( 1 - t ) * ( 1 - t ) ) * t * p1.y ) + ( 3 * ( 1 - t ) * ( t * t ) * p2.y ) + ( t * t * t ) * end.y;
-		out.z = ( ( 1 - t ) * ( 1 - t ) * ( 1 - t ) ) * start.z + ( 3 * ( ( 1 - t ) * ( 1 - t ) ) * t * p1.z ) + ( 3 * ( 1 - t ) * ( t * t ) * p2.z ) + ( t * t * t ) * end.z;
-
-		return	false;
+		//	Windows起動からの経過時間を取得
+		static DWORD lastTime = timeGetTime();
+		
+		//	前フレームからの経過時間を算出
+		DWORD elapseTime = timeGetTime() - lastTime;
+
+		//	現フレームの経過時間を加算
+		lastTime += elapseTime;
+		
+		//	経過時間をかえす
+		return	( float )elapseTime / 1000.0f;
 	}
 
 //----------------------------------------------------------------------------
@@ -593,144 +212,6 @@
 		return out;
 	}
 
-//----------------------------------------------------------------------
-//	補間関数用パラメータ更新
-//----------------------------------------------------------------------
-
-	//	割合更新( 更新したいパラメータ, 加算値 ) : 更新したいパラメータが1.0f以上の場合trueをかえす
-	bool	PercentageUpdate( float& param, float addParam )
-	{
-		//	パラメータ加算
-		param += addParam;
-
-		if ( param >= PERCENTAGE_MAX )
-		{
-			param = PERCENTAGE_MAX;
-			return	true;
-		}
-
-		return	false;
-	}
-
-//-------------------------------------------------------------------------
-//	図形描画
-//-------------------------------------------------------------------------
-
-	//	球描画
-	void	DrawSphere( LPDIRECT3DDEVICE9 d3dd, CONST D3DXVECTOR3 &p0, FLOAT r, D3DCOLOR c )
-	{
-		CONST INT N = 36;
-
-		static _VB vb( d3dd, sizeof( D3DXVECTOR3 ) * ( N + 1 ) );
-		for ( int n = 0; n < 3; n++ )
-		{
-			D3DXVECTOR3 *p;
-			vb->Lock( 0, 0, ( void ** )&p, 0 );
-			for ( int i = 0; i < N; i++ )
-			{
-				switch (  n )
-				{
-				case 0: p[i] = r * D3DXVECTOR3( sinf( 360.0f / N * 0.01745f * i ), cosf( 360.0f / N * 0.01745f * i ), 0 ) + p0; break;
-				case 1: p[i] = r * D3DXVECTOR3( sinf( 360.0f / N * 0.01745f * i ), 0, cosf( 360.0f / N * 0.01745f * i ) ) + p0; break;
-				case 2: p[i] = r * D3DXVECTOR3( 0, sinf( 360.0f / N * 0.01745f * i ), cosf( 360.0f / N * 0.01745f * i ) ) + p0; break;
-				}
-			}
-			p[N] = p[0];
-			vb->Unlock();
-
-			D3DMATERIAL9 m = { 0 };
-			m.Diffuse = m.Ambient = D3DXCOLOR( c );
-			d3dd->SetMaterial( &m );
-
-			d3dd->SetFVF( D3DFVF_XYZ );
-			d3dd->SetStreamSource( 0, vb, 0, sizeof( D3DXVECTOR3 ) );
-			d3dd->SetTransform( D3DTS_WORLD, &D3DXMATRIX( 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 ) );
-			d3dd->DrawPrimitive( D3DPT_LINESTRIP, 0, N );
-		}
-	}
-
-	void	DrawSphere( const Vector3& pos, float r, DWORD color )
-	{
-		iexSystem::GetDevice()->SetRenderState( D3DRS_ZENABLE, D3DZB_FALSE );
-		DrawSphere( iexSystem::GetDevice(), D3DXVECTOR3( pos.x, pos.y, pos.z ), r, color );
-		iexSystem::GetDevice()->SetRenderState( D3DRS_ZENABLE, D3DZB_TRUE );
-	}
-
-	//	カプセル描画
-	void	DrawCapsule( LPDIRECT3DDEVICE9 d3dd, CONST D3DXVECTOR3 &p0, CONST D3DXVECTOR3 &p1, FLOAT r, D3DCOLOR c )
-	{
-		CONST INT N = 36;
-		CONST INT L0 = 0;
-		CONST INT L1 = N + 1;
-		CONST INT L2 = L1 + N + 1;
-
-		CONST INT L3 = L2 + N * 2;
-		CONST INT L4 = L3 + N + 1;
-		CONST INT L5 = L4 + N + 1;
-		CONST INT L6 = L5 + N + 1;
-
-		static _VB vb(d3dd, sizeof(D3DXVECTOR3)* ((N + 1) * 2 + N * 2 + (N + 1) * 4));
-		D3DXVECTOR3 *p;
-		vb->Lock(0, 0, (void **)&p, 0);
-		D3DXVECTOR3 up = p0 - p1;
-		CONST FLOAT L = D3DXVec3Length(&up);
-		for (int i = 0; i < N; i++)
-		{
-			p[L0 + i] = D3DXVECTOR3(r * sinf(360.0f / N * 0.01745f * i), L / 2.0f, r * cosf(360.0f / N * 0.01745f * i));
-			p[L1 + i] = D3DXVECTOR3(r * sinf(360.0f / N * 0.01745f * i), -L / 2.0f, r * cosf(360.0f / N * 0.01745f * i));
-			if (i % (N / 4) == 0)
-			{
-				p[L2 + 2 * i] = p[L0 + i];
-				p[L2 + 2 * i + 1] = p[L1 + i];
-			}
-			p[L3 + i] = D3DXVECTOR3(r * cosf(180.0f / N * 0.01745f * i), r * sinf(180.0f / N * 0.01745f * i) + L / 2.0f, 0);
-			p[L4 + i] = D3DXVECTOR3(0, r * sinf(180.0f / N * 0.01745f * i) + L / 2.0f, r * cosf(180.0f / N * 0.01745f * i));
-			p[L5 + i] = D3DXVECTOR3(r * cosf(180.0f / N * 0.01745f * i), -r * sinf(180.0f / N * 0.01745f * i) - L / 2.0f, 0);
-			p[L6 + i] = D3DXVECTOR3(0, -r * sinf(180.0f / N * 0.01745f * i) - L / 2.0f, r * cosf(180.0f / N * 0.01745f * i));
-		}
-		p[L0 + N] = p[L0];
-		p[L1 + N] = p[L1];
-		p[L3 + N] = D3DXVECTOR3(-r, L / 2.0f, 0);
-		p[L4 + N] = D3DXVECTOR3(0, L / 2.0f, -r);
-		p[L5 + N] = D3DXVECTOR3(-r, L / 2.0f, 0);
-		p[L6 + N] = D3DXVECTOR3(0, L / 2.0f, -r);
-
-		vb->Unlock();
-
-		D3DXVECTOR3 axis;
-		D3DXVec3Cross(&axis, &D3DXVECTOR3(0, 1, 0), &up);
-		D3DXVec3Normalize(&axis, &axis);
-		FLOAT angle = acosf(D3DXVec3Dot(&up, &D3DXVECTOR3(0, 1, 0)) / D3DXVec3Length(&up));
-		D3DXMATRIX M;
-		D3DXMatrixRotationAxis(&M, &axis, angle);
-		D3DXVECTOR3 g = (p0 + p1) / 2;
-		M._41 = g.x;
-		M._42 = g.y;
-		M._43 = g.z;
-
-		D3DMATERIAL9 m = { 0 };
-		m.Diffuse = m.Ambient = D3DXCOLOR(c);
-		d3dd->SetMaterial(&m);
-
-		d3dd->SetFVF(D3DFVF_XYZ);
-		d3dd->SetStreamSource(0, vb, 0, sizeof(D3DXVECTOR3));
-		d3dd->SetTransform(D3DTS_WORLD, &M);
-		d3dd->DrawPrimitive(D3DPT_LINESTRIP, L0, N);
-		d3dd->DrawPrimitive(D3DPT_LINESTRIP, L1, N);
-		d3dd->DrawPrimitive(D3DPT_LINELIST, L2, N);
-		d3dd->DrawPrimitive(D3DPT_LINESTRIP, L3, N);
-		d3dd->DrawPrimitive(D3DPT_LINESTRIP, L4, N);
-		d3dd->DrawPrimitive(D3DPT_LINESTRIP, L5, N);
-		d3dd->DrawPrimitive(D3DPT_LINESTRIP, L6, N);
-	}
-
-	void	DrawCapsule( const Vector3& p1, const Vector3& p2, float r, DWORD color )
-	{
-		iexSystem::GetDevice()->SetRenderState( D3DRS_ZENABLE, D3DZB_FALSE );
-		DrawCapsule( iexSystem::GetDevice(), D3DXVECTOR3( p1.x, p1.y, p1.z ), D3DXVECTOR3( p2.x, p2.y, p2.z ), r, color );
-		iexSystem::GetDevice()->SetRenderState( D3DRS_ZENABLE, D3DZB_TRUE );
-	}
-
 //----------------------------------------------------------------------------
 //	図形設定
 //----------------------------------------------------------------------------
@@ -769,7 +250,7 @@
 		float tx = end.x - start.x;	//　X座標差
 		float tz = end.z - start.z;	//　Z座標差
 
-		float txz = sqrtf(tx*tx + tz*tz);
+		float txz = sqrtf( tx * tx + tz * tz );
 
 		// y = v0 * sinθ*t + 0.5f*G*t*t
 		// x = v0 * cosθ*t
--- IEX2010/source/GlobalFunction.h
+++ IEX2010/source/GlobalFunction.h
@@ -12,29 +12,10 @@
 //	定数
 //----------------------------------------------------------------------
 
-	#define	ZERO_VECTOR3		Vector3( 0.0f, 0.0f, 0.0f )
-	#define	FOR( minValue, maxValue )	for( int value = minValue; value < maxValue; value++ )
-	#define	FOR_LIST( minValue, maxValue )	for( auto it = minValue; it != maxValue; it++ )
-
-	namespace IMAGE_MODE
-	{
-		enum
-		{
-			NORMAL,			//	オリジナル描画
-			ADOPTPARAM,	//	パラメータ採用
-			ADD,			//	加算合成
-			WAVE,				//	波紋
-			FLASH,				//	点滅
-			SCALING,			//	拡大縮小
-			END,					//	終端
-		};
-	}
-
-//----------------------------------------------------------------------
-//	変数
-//----------------------------------------------------------------------
-	
-	extern	bool					debug;
+#define	ZERO_VECTOR3		Vector3( 0.0f, 0.0f, 0.0f )
+#define	FOR( minValue, maxValue )	for( int value = minValue; value < maxValue; value++ )
+#define	FOR_LIST( minValue, maxValue )	for( auto it = minValue; it != maxValue; it++ )
+#define	SUM( value ) ( value / 2 ) * ( value + 1 )
 
 //----------------------------------------------------------------------
 //	構造体
@@ -50,60 +31,9 @@
 		LPDIRECT3DVERTEXBUFFER9		operator -> (){ return p; }
 	};
 
-	//	画像構造体
-	struct ImageObj
-	{
-		iex2DObj*	obj;
-		int	 x, y, w, h;
-		int	 sx, sy, sw, sh;
-		float	t;
-		float	alpha;
-		float	angle;
-		POINT	p;
-		bool	renderflag;
-		Vector3	color;
-
-		//	wave・scaling共通用パラメータ
-		int	 plusScaleX;
-		int	 plusScaleY;
-
-		//	wave用パラメータ
-		float	wavespeed;
-		float	waveAlpha;
-		bool	waveState;
-		bool	waverenderflag;
-
-		//	flashing用パラメータ
-		float	flashingSpeed;
-		float	flashingAlpha;
-		float	flashingRenderflag;
-		float	flashingParam;
-
-		//	scaling用パラメータ
-		float	scalingspeed;
-		float	scalingAlpha;
-		bool	scalingAlphaFlag;
-		bool	scalingState;
-		bool	scalingFlag;
-		bool	scalingrenderflag;
-	};
-
 //----------------------------------------------------------------------
 //	システム
 //----------------------------------------------------------------------
-
-	//	読み込み ポインターをnull初期化してnewする
-	//	成功していたらtrueをかえす
-	template<typename T>
-	bool	PointerNew( T*& ptr, LPSTR filename = nullptr )
-	{
-		ptr = nullptr;
-		if ( filename != nullptr )	ptr = new T( filename );
-		else ptr = new T();
-
-		if ( ptr != nullptr )	return	true;
-		return	false;
-	}
 
 	//	解放 ポインターをdeleteしてnullptrを代入する
 	template<typename T>
@@ -127,10 +57,8 @@
 		}
 	}
 
-	//	デバッグ文字描画
-	void	DrawString( LPSTR string, int x, int y, DWORD color = 0xFFFFFFFF );
-	void	DrawString( LPSTR string, int x, int y, float r, float g, float b );
-	void	DrawString( LPSTR string, int x, int y, Vector3 color );
+	//	前フレームからの経過時間取得
+	float GetElapseTime( void );
 
 //----------------------------------------------------------------------
 //	変換
@@ -149,42 +77,11 @@
 	bool	WorldToClient( const Vector3& pos, Vector3& out, const Matrix& mat );
 
 	//	クライアント座標からワールド座標への変換
-	//	outにスクリーン上のワールド座標をかえす
-	void	ClientToWorld( const Vector3& screenPos, Vector3& out );
-
-//----------------------------------------------------------------------
-//	画像操作
-//----------------------------------------------------------------------
-
-	//	画像初期化・描画
-	void	ImageInitialize( ImageObj& image, int x, int y, int w, int h, int sx, int sy, int sw, int sh );
-	void	RenderImage( ImageObj image, int sx, int sy, int sw, int sh, int mode );
-	void	RenderImage( ImageObj image, int sx, int sy, int sw, int sh, int mode, int x, int y );
-
-	//	波紋の設定(波紋出したいオブジェクト、波紋スピード)
-	void	SetWave( ImageObj& image, float speed );
-
-	//	波紋更新（波紋出したいオブジェクト、波紋大きさ、スタート時透明度）波紋終了時trueをかえす
-	bool	WaveUpdate( ImageObj& image, int max_scale = 140, float max_alpha = 1.0f );
-
-	//	点滅更新（点滅させたいオブジェクト、点滅スピード）
-	void	FlashingUpdate( ImageObj& image, float speed = -1.0f );
-
-	//	拡大
-	void	SetScaling( ImageObj& image, float speed, bool state = true );					//	state:	true = 拡大　false = 縮小
-	void	ScalingUpdate(ImageObj& image, int max_scale = 140);
-	void	ScalingUpdate2(ImageObj& image, int max_scale = 140);
-	bool	ScalingAlphaUpdate(ImageObj& image, int max_scale = 140);
-	void	ScalingLandingUpdate(ImageObj& image, int max_scale = 140);
-	void	StopScaling( ImageObj& image );
+	//	変換後のワールド座標、カメラの奥への向き、スクリーン座標
+	void	ClientToWorld( Vector3& outPos, Vector3& outVec, POINT screenPos );
 	
-//----------------------------------------------------------------------
-//	図形描画
-//----------------------------------------------------------------------
-	void	DrawSphere( LPDIRECT3DDEVICE9 d3dd, CONST D3DXVECTOR3 &p0, FLOAT r, D3DCOLOR c = 0xFFFFFFFF );
-	void	DrawSphere( const Vector3& pos, float r, DWORD color = 0xFFFFFFFF );
-	void	DrawCapsule( LPDIRECT3DDEVICE9 d3dd, CONST D3DXVECTOR3 &p1, CONST D3DXVECTOR3 &p2, FLOAT r, D3DCOLOR c = 0xFFFFFFFF );
-	void	DrawCapsule( const Vector3& p1, const Vector3& p2, float r, DWORD color = 0xFFFFFFFF );
+	//	レイを当てるメッシュ、出力、スクリーン座標
+	void	ClientToWorld( iexMesh* targetMesh, Vector3& out, const POINT& screenPos );
 
 //----------------------------------------------------------------------
 //	図形設定
@@ -195,43 +92,6 @@
 
 	//	頂点設定
 	void	SetVertex( TLVERTEX& v, float x, float y, float z, float tu, float tv, DWORD color );
-
-//----------------------------------------------------------------------
-//	線形補間( 出力、開始値、最終値, 割合 )　終了時trueをかえす
-//----------------------------------------------------------------------
-
-	template<typename T, typename T2>
-	bool	Lerp( T& out, T2 p1, T2 p2, float t )
-	{
-		out = ( T )( p1 * ( 1 - t ) + p2 * t );
-
-		if ( t >= 1.0f )	return	true;
-		return	false;
-	}
-
-//----------------------------------------------------------------------
-//	３次関数補間( 出力、開始値、最終値, 割合 )　終了時trueをかえす
-//----------------------------------------------------------------------
-
-	template<typename T, typename T2>
-	bool	CubicFunctionInterpolation( T& out, T2 p1, T2 p2, float t )
-	{
-		float rate = t * t * ( 3.0f - 2.0f * t );   // 3次関数補間値に変換
-		out = ( T )( p1 * ( 1.0f - rate ) + p2 * rate );
-
-		if ( t >= 1.0f )	return	true;
-		return	false;
-	}
-
-//----------------------------------------------------------------------
-//	ベジェ曲線（出力、スタート位置、制御点、終了位置、求めたい割合） 終了時trueをかえす
-//----------------------------------------------------------------------
-
-	//	２次ベジェ曲線
-	bool	BezierCurve( Vector3& out, Vector3 start, Vector3 p1, Vector3 end, float t );
-
-	//	３次ベジェ曲線
-	bool	BezierCurve( Vector3& out, Vector3 start, Vector3 p1, Vector3 p2, Vector3 end, float t );
 
 	//	始点終点の動作種類
 	namespace ePrm_t
@@ -253,13 +113,6 @@
 
 	//	高次元ベジェ曲線パラメータ取得( 0.0f ~ 1.0f )
 	float GetBezier( int ePrm1, int ePrm2, float fRate );
-
-//----------------------------------------------------------------------
-//	補間関数用パラメータ更新
-//----------------------------------------------------------------------
-
-	//	割合更新( 更新したいパラメータ, 加算値 ) : 更新したいパラメータが1.0f以上の場合trueをかえす
-	bool	PercentageUpdate( float& param, float addParam );
 
 //----------------------------------------------------------------------
 //	放物線(出力、開始点、目標地点、初速、重力)

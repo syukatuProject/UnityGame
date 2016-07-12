2adaa788d94e3e0a0e8edcd9b3391d4daa82004d
高鳥義行 <wtpmjgdadgjptw@gmail.com>
2016-07-12 22:59:54 +0900

    no message

---
+++ IEX2010/source/GameManager.h
@@ -0,0 +1,27 @@
+
+#pragma once
+
+//**********************************************************************
+//
+//	GameManagerクラス
+//
+//**********************************************************************
+
+//	include	
+
+//	class
+class GameManager
+{
+private:
+public:
+	//	初期化・解放
+	GameManager( void );
+	~GameManager( void );
+
+	//	更新
+	void	Update( void );
+
+	//	情報設定
+	
+	//	情報取得
+};
---
+++ IEX2010/source/UIManager.h
@@ -0,0 +1,31 @@
+
+#pragma once
+
+//**********************************************************************
+//
+//	UIManagerクラス
+//
+//**********************************************************************
+
+//	include
+
+//	class
+class UIManager
+{
+private:
+public:
+	//	初期化・解放
+	UIManager( void );
+	~UIManager( void );
+
+	//	更新・描画
+	void	Update( void );
+	void	Render( void );
+
+	//	動作関数
+	
+	//	情報設定
+
+	//	情報取得
+
+};
